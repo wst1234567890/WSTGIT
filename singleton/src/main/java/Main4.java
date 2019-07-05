cb5bf66395ea30e49467d98a9842bd94682cf33b
wangsongtao <932356624@qq.com>
2019-07-05 14:04:41 +0800

    singleten添加main4

---
+++ singleton/src/main/java/Main4.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName: Main3
+ * @PackAgeName: PACKAGE_NAME
+ * @Author: WST
+ * @Date: 2019/6/28  16:31
+ * @VerSion: 1.0
+ **/
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println(SingleTon3.singTon3==SingleTon3.singTon3);
+    }
+}
+enum  SingleTon3{//ц╤╬ый╣ож
+    singTon3
+}
